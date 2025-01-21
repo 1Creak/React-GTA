c867c9c836919e3bc364e6fb18c5c8566ffd47ce
1Creak <88380622+1Creak@users.noreply.github.com>
2025-01-21 16:01:01 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 import Counter from "./components/Counter.jsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/Register.jsx';
+import { Link } from 'react-router-dom';
 
 function App() {
+    return (
+        <>
+            <Router>
+                <nav>
+                    <Link to="/about">Go to Register Page</Link> |
+                    <Link to="/home">Go to Home Page</Link>
+                </nav>
 
-  return (
-    <>
-        <Counter/>
-    </>
-  )
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </Router>
+
+            <Counter />
+        </>
+    );
 }
 
-export default App
+export default App;
