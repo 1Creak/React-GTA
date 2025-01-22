4ae1b91d61fcdb102f4fe5eaa6a3c75134b17440
1Creak <88380622+1Creak@users.noreply.github.com>
2025-01-22 16:47:19 +0200

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route,useLocation, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/register/Register.jsx';
 import Login from './pages/login/Login.jsx';
-import {Link} from 'react-router-dom';
 import UserInfo from "./components/userInfo/UserInfo.jsx";
 
-function App() {
+function Navigation() {
     const location = useLocation();
     return (
-        <>
+        <header>
+            {location.pathname !== '/home' && <UserInfo />}
+            <nav className='navigation'>
+                <Link to="/register">Go to Register Page</Link> |
+                <Link to="/login">Go to Login Page</Link> |
+                <Link to="/home">Go to Home Page</Link>
+            </nav>
+        </header>
+    );
+}
 
-            <Router>
-                <header>
-                    {/*{location.pathname !== '/home' && <UserInfo />}*/}
-                    <nav className='navigation'>
-                        <Link to="/register">Go to Register Page</Link> |
-                        <Link to="/login">Go to Login Page</Link> |
-                        <Link to="/home">Go to Home Page</Link>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path="/home" element={<Home/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<About/>}/>
-                </Routes>
-            </Router>
-
-        </>
-    )
-        ;
+function App() {
+    return (
+        <Router>
+            <Navigation />
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<About />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
